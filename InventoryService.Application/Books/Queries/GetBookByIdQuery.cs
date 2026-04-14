bd7a93980a4e78dd255bccd83be692c4e46a40a8
emedinalopez <emedinalopez@deloitte.com>
2026-04-14 02:22:21 -0600

    GetBookById
    Implemented Query and Handler for GetBookById

---
+++ InventoryService.Application/Books/Queries/GetBookByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using InventoryService.Application.DTOs;
+using MediatR;
+
+namespace InventoryService.Application.Books.Queries
+{
+    public class GetBookByIdQuery : IRequest<BookDTO?>
+    {
+        public int BookId { get; set; }
+    }
+}
---
+++ InventoryService.Application/Books/Queries/GetBookByIdQueryHandler.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using InventoryService.Application.DTOs;
+using InventoryService.Application.Interfaces;
+
+namespace InventoryService.Application.Books.Queries
+{
+    public class GetBookByIdQueryHandler : IRequestHandler<GetBookByIdQuery, BookDTO?>
+    {
+        private readonly IInventoryDbContext _context;
+
+        public GetBookByIdQueryHandler(IInventoryDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<BookDTO?> Handle(GetBookByIdQuery request, CancellationToken cancellationToken)
+        {
+            var book = await _context.Books
+                .AsNoTracking()
+                .Include(b => b.Category)
+                .Where(b => b.Id == request.BookId)
+                .Select(b => new BookDTO
+                {
+                    ID = request.BookId,
+                    Title = b.Title,
+                    Author = b.Author,
+                    ISBN = b.ISBN,
+                    Price = b.Price,
+                    StockQty = b.StockQty,
+                    CategoryId = b.CategoryId                  
+                })
+                .FirstOrDefaultAsync(cancellationToken);
+
+            return book;
+        }
+    }
+}
