dc873d7b67d243c6bd1f6cc63f63a2031ed9d9f7
emedinalopez <emedinalopez@deloitte.com>
2026-04-14 15:54:11 -0600

    Entities + EF
    Created Order and OrderItem entities in the domain layer.
    Installed EF packages in Infrastructure layer

---
+++ OrderService.Domain/Entities/Order.cs
@@ -0,0 +1,14 @@
+﻿using OrderService.Domain.Enums;
+
+namespace OrderService.Domain.Entities
+{    
+    public class Order
+    {        
+        public int Id { get; set; }
+        public string CustomerName { get; set; } = string.Empty;
+        public DateTime OrderDate { get; set; }
+        public OrderStatus Status { get; set; }
+        public decimal TotalAmount { get; set; }
+        public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+    }
+}
---
+++ OrderService.Domain/Entities/OrderItem.cs
@@ -0,0 +1,13 @@
+﻿namespace OrderService.Domain.Entities
+{
+    public class OrderItem
+    {        
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public string BookTitle { get; set; } = string.Empty;
+        public decimal UnitPrice { get; set; }
+        public int Quantity { get; set; }
+        public int OrderId { get; set; }
+        public Order Order { get; set; } = null!;
+    }
+}
