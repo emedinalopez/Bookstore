de6ea6b09eb5fd450d1c8856f62777f96c5b4d33
emedinalopez <emedinalopez@deloitte.com>
2026-04-14 15:58:07 -0600

    IOrderDbContext
    Created interface for OrderDbContext in Application layer

---
+++ OrderService.Application/Interfaces/IOrderDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderService.Domain.Entities;
+
+namespace OrderService.Application.Interfaces
+{
+    public interface IOrderDbContext
+    {
+        DbSet<Order> Orders { get; }
+        DbSet<OrderItem> OrderItems { get; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
