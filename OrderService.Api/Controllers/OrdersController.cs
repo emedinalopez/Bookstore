4fbb5cda588d5157ddfcfc8f48600bb05f579389
emedinalopez <emedinalopez@deloitte.com>
2026-04-15 13:56:28 -0600

    OrdersController
    Added intial OrdersController

---
+++ OrderService.Api/Controllers/OrdersController.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using OrderService.Application.Orders.Commands;
+using OrderService.Application.DTOs;
+
+namespace OrderService.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public OrdersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<OrderDTO>> CreateOrder([FromBody] CreateOrderCommand command)
+        {
+            var createdOrder = await _mediator.Send(command);
+                        
+            return Ok(createdOrder);
+        }        
+    }
+}
---
+++ OrderService.Application/DTOs/OrderDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderService.Application.DTOs
+{
+    public class OrderDTO
+    {
+    }
+}
