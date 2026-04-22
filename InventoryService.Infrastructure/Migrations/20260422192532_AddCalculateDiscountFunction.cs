4810fa7f41f05657242a07e0eba0f23bf3b90afe
emedinalopez <emedinalopez@deloitte.com>
2026-04-22 13:27:44 -0600

    discount function
    Added discount function

---
+++ InventoryService.Infrastructure/Migrations/20260422192532_AddCalculateDiscountFunction.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCalculateDiscountFunction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                CREATE OR ALTER FUNCTION dbo.fn_CalculateDiscountedPrice
+                (
+                    @OriginalPrice DECIMAL(18,2),
+                    @DiscountPercentage DECIMAL(5,2)
+                )
+                RETURNS DECIMAL(18,2)
+                AS
+                BEGIN
+                    DECLARE @DiscountedPrice DECIMAL(18,2);
+                    SET @DiscountedPrice = @OriginalPrice - (@OriginalPrice * (@DiscountPercentage / 100.0));
+                    RETURN @DiscountedPrice;
+                END
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP FUNCTION IF EXISTS dbo.fn_CalculateDiscountedPrice");
+        }
+    }
+}
