763d82c452ac6f1126e08a8588fcde20b54733f3
emedinalopez <emedinalopez@deloitte.com>
2026-04-22 12:45:02 -0600

    GetTopBooks
    Added migration to add stored proc GetTopBooks

---
+++ InventoryService.Infrastructure/Migrations/20260422182142_AddTopSellingBooksStoredProcedure.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTopSellingBooksStoredProcedure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                CREATE OR ALTER PROCEDURE dbo.usp_GetTopSellingBooks
+                    @TopN INT = 5
+                AS
+                BEGIN
+                    SET NOCOUNT ON;
+
+                    -- Create temp table to hold aggregated results
+                    CREATE TABLE #TopBooks (
+                        BookId INT,
+                        BookTitle NVARCHAR(200),
+                        TotalQuantitySold INT
+                    );
+
+                    -- Insert aggregated data into the temporary table
+                    INSERT INTO #TopBooks (BookId, BookTitle, TotalQuantitySold)
+                    SELECT 
+                        BookId, 
+                        MAX(BookTitle),
+                        SUM(Quantity)
+                    FROM 
+                        dbo.OrderItems
+                    GROUP BY 
+                        BookId;
+                    
+                    SELECT TOP (@TopN) 
+                        BookId, 
+                        BookTitle, 
+                        TotalQuantitySold
+                    FROM 
+                        #TopBooks
+                    ORDER BY 
+                        TotalQuantitySold DESC;
+
+                    DROP TABLE #TopBooks;
+                END
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP PROCEDURE IF EXISTS dbo.usp_GetTopSellingBooks");
+        }
+    }
+}
