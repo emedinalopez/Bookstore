4ce307719758d590ef5e3d48f299d1d8a59bc894
emedinalopez <emedinalopez@deloitte.com>
2026-04-30 09:09:38 -0600

    Added a ValidationException

---
+++ Bookstore.Common/Exceptions/ValidationException.cs
@@ -0,0 +1,28 @@
+﻿namespace Bookstore.Common.Exceptions
+{
+    public class ValidationException : Exception
+    {        
+        public IDictionary<string, string[]> Errors { get; }
+     
+        public ValidationException(string message)
+            : base(message)
+        {
+            Errors = new Dictionary<string, string[]>();
+        }
+        
+        public ValidationException(IDictionary<string, string[]> errors)
+            : base("One or more validation failures have occurred.")
+        {
+            Errors = errors;
+        }
+        
+        public ValidationException(string propertyName, string errorMessage)
+            : base("Validation failed.")
+        {
+            Errors = new Dictionary<string, string[]>
+            {
+                { propertyName, new[] { errorMessage } }
+            };
+        }
+    }
+}
